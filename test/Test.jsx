cf762e35c39971912e14dbc3f89f04f4d2b9e983
Michał Wiaderek <wiaderekmichal@gmail.com>
2019-08-03 22:32:13 +0200

    Docs updated

--- test/Test.jsx
+++ test/Test.jsx
@@ -1,10 +1,10 @@
 import React, { PureComponent } from 'react';
-import PromiseButton from '@mivia/promise-button';
+import PromiseButton from '@mivia/react-promise-button';
 
-import BUTTON_STATES from '../src/constants';
+import './Test.less';
 
 const pendingConfig = {
-  className: `PromiseButton--${BUTTON_STATES.PENDING}`,
+  className: 'orange',
   children: (
     <span>
       Pending!
@@ -13,7 +13,7 @@ const pendingConfig = {
 };
 
 const successConfig = {
-  className: `PromiseButton--${BUTTON_STATES.SUCCESS}`,
+  className: 'green',
   children: (
     <span>
       Success done!
@@ -22,7 +22,7 @@ const successConfig = {
 };
 
 const errorConfig = {
-  className: `PromiseButton--${BUTTON_STATES.ERROR}`,
+  className: 'red',
   children: (
     <span>
       Error done!
@@ -43,32 +43,88 @@ export default class Test extends PureComponent {
     }, 2000);
   })
 
+  renderNoPendingConfigButton = () => (
+    <>
+      <p>
+        Callback function returning resolving promise triggers only success config when pending config was not provided
+      </p>
+      <PromiseButton
+        className="blue"
+        onClick={this.onClickSuccess}
+        successConfig={successConfig}
+      >
+      No pending config
+      </PromiseButton>
+    </>
+  )
+
+  renderNoPromiseButton = () => (
+    <>
+      <p>
+        Callback function that does not return a promise does not crash the button
+         - callback function is turned into immidiate resolving promise:
+      </p>
+      <PromiseButton
+        className="blue"
+        onClick={() => {}}
+        pendingConfig={pendingConfig}
+        successConfig={successConfig}
+      >
+      No promise
+      </PromiseButton>
+    </>
+  )
+
   renderSuccessButton = () => (
-    <PromiseButton
-      onClick={this.onClickSuccess}
-      pendingConfig={pendingConfig}
-      successConfig={successConfig}
-    >
+    <>
+      <p>
+        Callback function returning resolving promise triggers pending & success config
+      </p>
+      <PromiseButton
+        className="blue"
+        onClick={this.onClickSuccess}
+        pendingConfig={pendingConfig}
+        successConfig={successConfig}
+      >
       Gonna be success
-    </PromiseButton>
+      </PromiseButton>
+    </>
   )
 
   renderErrorButton = () => (
-    <PromiseButton
-      className="check"
-      errorConfig={errorConfig}
-      onClick={this.onClickError}
-      pendingConfig={pendingConfig}
-    >
+    <>
+      <p>
+        Callback function returning rejecting promise triggers pending & error config
+      </p>
+      <PromiseButton
+        className="blue"
+        errorConfig={errorConfig}
+        onClick={this.onClickError}
+        pendingConfig={pendingConfig}
+      >
       Gonna be error
-    </PromiseButton>
+      </PromiseButton>
+    </>
   )
 
   render() {
     return (
       <>
-        {this.renderSuccessButton()}
-        {this.renderErrorButton()}
+        <h2>React-Promise-Button preview</h2>
+        <div className="Container">
+          <div className="Button-wrapper">
+            {this.renderSuccessButton()}
+          </div>
+          <div className="Button-wrapper">
+            {this.renderErrorButton()}
+          </div>
+          <div className="Button-wrapper">
+            {this.renderNoPromiseButton()}
+          </div>
+          <div className="Button-wrapper">
+            {this.renderNoPendingConfigButton()}
+          </div>
+        </div>
       </>
     );
   }
